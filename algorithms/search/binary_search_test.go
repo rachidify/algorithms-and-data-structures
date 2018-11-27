5d6ef34abc00b6df3e36aebd2df411f98c9223a2
= <rasheedhamdawi@gmail.com>
2018-11-27 16:03:05 +0100

    add bubble sort

--- algorithms/search/binary_search_test.go
+++ algorithms/search/binary_search_test.go
@@ -15,14 +15,12 @@ func init() {
 }
 
 func TestBinarySearch(t *testing.T) {
-
 	if result := search.BinarySearch(list, 8); result != 4 {
 		t.Error("Expected 4, got", result)
 	}
 }
 
 func BenchmarkBinarySearch(b *testing.B) {
-
 	for i := 0; i < b.N; i++ {
 		search.BinarySearch(list, 8)
 	}
